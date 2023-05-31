d5cfccc387fac8010492d3a6addf78c35da6f9ff
pauloivoandrade <pauloivoasantos@gmail.com>
2023-05-31 10:41:42 -0300

    Exercicio5.2

---
+++ fundamentos/Secao-03-Introducao-a-JavaScript/exercicio5.2.js
@@ -0,0 +1,5 @@
+var num1 = 60;
+var num2 = 40;
+maiorNumero = "";
+if (num1 > num2){maiorNumero=num1} else if (num1<num2) {maiorNumero = num2} else {maiorNumero="Numeros Iguais"}
+console.log(maiorNumero);
