41df086a9a3b6d8d340926014d694a7585325dc9
pauloivoandrade <pauloivoasantos@gmail.com>
2023-05-31 11:26:34 -0300

    fix exercicio5.4 e inicio exercicio 5.6

--- fundamentos/Secao-03-Introducao-a-JavaScript/exercicio5.4.js
+++ fundamentos/Secao-03-Introducao-a-JavaScript/exercicio5.4.js
@@ -1,6 +1,6 @@
-var angulo1 = 25;
-var angulo2 = 10;
-var angulo3 = 65;
+var angulo1 = 60;
+var angulo2 = 60;
+var angulo3 = 0;
 var triangulo = true;
-if(angulo1+angulo2+angulo3===180){triangulo=true} else{triangulo=false}
+if(angulo1+angulo2+angulo3===180){triangulo=true} else if (angulo1<=0 || angulo2 <=0 || angulo3<= 0) {triangulo = "ERRO"}else{triangulo=false}
 console.log(triangulo);
---
+++ fundamentos/Secao-03-Introducao-a-JavaScript/exercicio5.6.js
@@ -0,0 +1,6 @@
+var num1 = 4;
+var num2 = 4;
+var num3 = 8;
+var numeroPar ="";
+if (num1/2 % 0 || num2/2 % 0 || num3/2 % 0) {numeroPar = true} else {numeroPar = false}
+console.log(numeroPar);
