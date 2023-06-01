7e1ef9cb884544913f5b07f3caeaa2cef401cb6e
pauloivoandrade <pauloivoasantos@gmail.com>
2023-06-01 16:00:59 -0300

    Desafio-Asterisco

---
+++ fundamentos/Secao-03-Introducao-a-JavaScript/dia-02-Array e estrutura de repetição/Desafio-dos-Asteriscos.js
@@ -0,0 +1,11 @@
+let n = 5;
+let box = [];
+let symbol= "";
+
+for (let index = n; index >= 1; index -= 1){
+    symbol = symbol + "*"
+} 
+for(let index = n; index >= 1; index -= 1){
+    console.log(symbol)
+}
+    
