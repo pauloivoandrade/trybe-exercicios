2e2faf6e0c94c021ac09fb8831456c2afd58bbd9
pauloivoandrade <pauloivoasantos@gmail.com>
2023-06-01 13:28:40 -0300

    Multiplicando index segundo FOR

--- fundamentos/Secao-03-Introducao-a-JavaScript/dia-02-Array e estrutura de repetição/Desafio-Fatorial.js
+++ fundamentos/Secao-03-Introducao-a-JavaScript/dia-02-Array e estrutura de repetição/Desafio-Fatorial.js
@@ -3,9 +3,13 @@ let number = 10;
 let fatorial;
 
 for(let index = 10; index >= 1; index -= 1){
-    fator.push(index)
-    
-    
-}
+    fator.push(index)}
+            for(let index = 1; index <= fator.length; index * (index+= 1)){
 
-console.log(fator);
+               fatorial = index * (index+=1)
+        
+    }
+    console.log(fatorial);
+    
+
+
