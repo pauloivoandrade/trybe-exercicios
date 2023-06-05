7b9cdf8e846cdf5c2c2ae5b54890e06175bd4fc4
pauloivoandrade <pauloivoasantos@gmail.com>
2023-06-05 20:36:14 -0300

    Exercicio 04 DUVIDAS

---
+++ fundamentos/Secao-03-Introducao-a-JavaScript/dia-04-Objetos/Manipulação de objetos: Parte 4.js
@@ -0,0 +1,39 @@
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+  ];
+  
+
+  function separarElementosIguais(array) {
+    const elementosSeparados = {};
+    
+    for (let index = 0; index < array.length; index += 1) {
+      const elemento = array[index];
+      
+      if (elementosSeparados[elemento]) {
+        elementosSeparados[elemento].push(elemento);
+      } else {
+        elementosSeparados[elemento] = [elemento];
+      }
+    }
+    
+    return Object.values(elementosSeparados);
+  }
+  const elementosSeparados = {
+    Melancia:
+  }
