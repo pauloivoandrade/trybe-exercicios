e2fbf1a1ef50847bbc4b7be7f24122354d5dd997
pauloivoandrade <pauloivoasantos@gmail.com>
2023-06-14 09:52:52 -0300

    Exercicios 01 e 02

---
+++ fundamentos/Secao-04-JS-DOM/exercicio-01-Buscando-Elementos/script.js
@@ -0,0 +1,6 @@
+// arquivo script.js
+
+// console.log(document.getElementById('start').nextSibling) // nó
+
+// console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+const cadeVoce = document.getElementById("where-are-you");
