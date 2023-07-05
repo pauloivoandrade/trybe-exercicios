e264d9e6b34e22895e089673b4fd9f5cb975e9fc
pauloivoandrade <pauloivoasantos@gmail.com>
2023-07-05 11:13:02 -0300

    Feat: Npm Tests

--- fundamentos/Secao-06-JavaES6-TestesUnitarios/Dia-03-Matchers-e-Cobertura/Fixacao-testes/createItem.test.js
+++ fundamentos/Secao-06-JavaES6-TestesUnitarios/Dia-03-Matchers-e-Cobertura/Fixacao-testes/createItem.test.js
@@ -1,10 +1,22 @@
 const createItem = require('./createItem');
 
 describe('a função createItem', () => {
-  it.todo('cria um item válido');
-  it.todo('utiliza zero como quantidade padrão');
-  it.todo('Lança um erro quando não recebe parâmetros');
-  it.todo('Lança um erro se o nome do item não é uma string');
-  it.todo('Lança um erro se o preço é negativo');
-  it.todo('Lança um erro se o preço é zero');
+    it('cria um item válido', () => {
+        expect(createItem('banana', 'kg', 1.99, 20)).toEqual({ name: 'banana', unit: 'kg', price: 1.99, quantity: 20 })
+      });
+  it('utiliza zero como quantidade padrão', () => {
+    expect(createItem('banana', 'kg', 1.99)).toEqual({ name: 'banana', unit: 'kg', price: 1.99, quantity: 0})
+})
+  it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => createItem()).toThrow(Error);
+  });
+  it('Lança um erro se o nome do item não é uma string', () => {
+    expect(() => createItem(1.99, 'kg', 'banana', 3,55)).toThrow(Error);
+  });
+  it('Lança um erro se o preço é negativo', () => {
+    expect(() => createItem('banana', 'kg', -1.99, 7)).toThrow(Error);
+  });
+  it('Lança um erro se o preço é zero', () => {
+    expect(() => createItem('banana', 'kg', 0, 7)).toThrow(Error);
+  });
 });
